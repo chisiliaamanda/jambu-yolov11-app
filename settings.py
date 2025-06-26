859c78a6f26f1a251f7f18834e6ef187fbdeef25
chisiliaamanda <chisiliaamanda123@gmail.com>
2025-06-26 13:58:32 +0700

    Fix settings.py for jambu biji detection

--- settings.py
+++ settings.py
@@ -3,13 +3,11 @@ import sys
 
 # Get the absolute path of the current file
 FILE = Path(__file__).resolve()
-# Get the parent directory of the current file
 ROOT = FILE.parent
+
 # Add the root path to the sys.path list if it is not already there
-if ROOT not in sys.path:
+if str(ROOT) not in sys.path:
     sys.path.append(str(ROOT))
-# Get the relative path of the root directory with respect to the current working directory
-ROOT = ROOT.relative_to(Path.cwd())
 
 # Sources
 IMAGE = 'Image'
@@ -18,30 +16,24 @@ WEBCAM = 'Webcam'
 RTSP = 'RTSP'
 YOUTUBE = 'YouTube'
 
-SOURCES_LIST = [IMAGE]
+SOURCES_LIST = [IMAGE, VIDEO, WEBCAM, RTSP, YOUTUBE]
 
 # Images config
 IMAGES_DIR = ROOT / 'images'
-DEFAULT_IMAGE = IMAGES_DIR / 'apple.jpg'
-DEFAULT_DETECT_IMAGE = IMAGES_DIR / 'apple detection.jpg'
+DEFAULT_IMAGE = IMAGES_DIR / 'jambu1.jpg'
+DEFAULT_DETECT_IMAGE = IMAGES_DIR / 'detectedimage1.png'
 
 # Videos config
-VIDEO_DIR = ROOT / 'Videos'
+VIDEOS_DIR = ROOT / 'videos'
 VIDEOS_DICT = {
-    'video_1': VIDEO_DIR / '',
-    'video_2': VIDEO_DIR / '',
-    'video_3': VIDEO_DIR / '',
+    'video_1': VIDEOS_DIR / 'video_1.mp4',
+    'video_2': VIDEOS_DIR / 'video_2.mp4',
+    'video_3': VIDEOS_DIR / 'video_3.mp4',
 }
-
 
 # ML Model config
 MODEL_DIR = ROOT / 'weights'
 DETECTION_MODEL = MODEL_DIR / 'best.pt'
-# In case of your custome model comment out the line above and
-# Place your custom model pt file name at the line below 
-# DETECTION_MODEL = MODEL_DIR / 'my_detection_model.pt'
 
-SEGMENTATION_MODEL = MODEL_DIR / 'yolov8n-seg.pt'
-
-# Webcam
+# Webcam path
 WEBCAM_PATH = 0
